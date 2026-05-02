0b9eb441433200b39cd8d8533b07578df8dc6447
Shokhanasser1 <shokhanasserprojr@gmail.com>
2026-05-02 10:39:07 +0500

    admin adding

--- backend/apps/admin_api/apps.py
+++ backend/apps/admin_api/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class AdminApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.admin_api'
-    verbose_name = 'Admin API'
+    verbose_name = 'Admin API'   
   
